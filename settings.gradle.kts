0340ab5382f44872e730238eead3c41d0aed5dce
zhenjin.ma <zhenjin.ma@linktivity.co.jp>
2025-08-04 09:45:54 +0900

    save

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,6 +14,7 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        mavenLocal()// 可以在在本地发布的库中查找
         google()
         mavenCentral()
     }
